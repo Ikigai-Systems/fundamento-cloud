febaaf2dad76cc4bdbc929d9945b80a4b60780e9
Pawel Niewiadomski <11110000b@gmail.com>
2024-07-04 15:35:59 +0200

    Added Organization and OrganizationUser models

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_27_223112) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_04_131626) do
   create_table "documents", force: :cascade do |t|
     t.json "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.binary "sync"
+  end
+
+  create_table "organization_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.integer "organization_role", limit: 2, default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_organization_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_organization_users_on_reset_password_token", unique: true
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "spaces", force: :cascade do |t|
