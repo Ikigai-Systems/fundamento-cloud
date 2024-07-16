20da58e3a785e8b124eb27e007d71c5d74a42147
Pawel Niewiadomski <11110000b@gmail.com>
2024-07-16 15:59:31 +0200

    Disabled importmaps and included everything inside vite (hotwire/stimulus/controllers)

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -14,13 +14,15 @@ console.log('Vite ⚡️ Rails')
 console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
 
 // Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
+
+import * as Turbo from '@hotwired/turbo'
+Turbo.start()
+
+import * as ActiveStorage from '@rails/activestorage'
+ActiveStorage.start()
+
+import "~/controllers/application.js"
+
 // // Import all channels.
 // const channels = import.meta.globEager('./**/*_channel.js')
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import React from '@vitejs/plugin-react'
 import RubyPlugin from 'vite-plugin-ruby'
+import StimulusHMR from 'vite-plugin-stimulus-hmr'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
     React(),
     RubyPlugin(),
+    StimulusHMR(),
   ],
 })
