97f7aed01aa399513f5026bbf4e310655e22cfbf
Pawel Niewiadomski <11110000b@gmail.com>
2024-07-16 14:48:20 +0200

    Added tailwindcss-stimulus-components using importmap pin

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,6 +4,8 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/actioncable", to: "actioncable.esm.js"
+pin "tailwindcss-stimulus-components", preload: true # @5.1.1
+
+pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/channels", under: "channels"
