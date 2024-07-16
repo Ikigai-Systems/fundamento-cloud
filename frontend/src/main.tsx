e031dd904146f49943437d9101ad937649335c90
Piotr Stefaniak <stefan@ikigai.systems>
2024-07-16 20:02:32 +0200

    Removing UnoCSS from the codebase, making tailwindcss to work with tsx/jsx files as well

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {RouterProvider} from "react-router-dom";
 import './index.css'
-import 'virtual:uno.css'
 import router from "./router.tsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,11 @@
 import {defineConfig, loadEnv} from 'vite'
 import React from '@vitejs/plugin-react'
-import UnoCSS from 'unocss/vite'
-import {presetAttributify, presetIcons, presetUno} from "unocss";
 
 // https://vitejs.dev/config/
 export default defineConfig(({command, mode}) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [
-      UnoCSS({
-        presets: [
-          presetUno(),
-          presetAttributify(),
-          presetIcons(),
-        ]
-      }),
       React(),
     ],
 
