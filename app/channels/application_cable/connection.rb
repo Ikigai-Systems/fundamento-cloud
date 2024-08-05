f43c2f5f69ed61ac36928d279d3bfefb84f6977a
Pawel Niewiadomski <11110000b@gmail.com>
2024-08-05 14:30:17 +0200

    Action Cable Connection sets current_user and current_organization

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,17 +1,32 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
+    identified_by :current_organization
 
     def connect
-      # self.current_user = find_verified_user
-      self.current_user = User.first
+      self.current_user = load_current_user
+      self.current_organization = load_current_organization
+    end
+
+    def around_command
+      # This method can be used to set thread local context for the time given command is processed
+      # with_tenant(self.organization) { yield }
     end
 
     private
 
-    def find_verified_user
-      if (verified_user = User.find_by(id: cookies.encrypted[:user_id]))
-        verified_user
+    def load_current_user
+      if (current_user = env['warden'].user)
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+
+    def load_current_organization
+      if (current_organization = Organization.find_by_id(cookies.encrypted[:organization_id])) \
+        && current_user.organizations.include?(current_organization)
+        current_organization
       else
         reject_unauthorized_connection
       end
