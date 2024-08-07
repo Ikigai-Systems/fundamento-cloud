fac1fbd2bd51d5188e73b674b80dd1a79dead9b0
Pawel Niewiadomski <11110000b@gmail.com>
2024-08-07 10:29:55 +0200

    Added space_id to documents table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_05_125328) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_07_082901) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,7 +31,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_05_125328) do
     t.binary "sync"
     t.string "title"
     t.bigint "organization_id"
+    t.bigint "space_id"
     t.index ["organization_id"], name: "index_documents_on_organization_id"
+    t.index ["space_id"], name: "index_documents_on_space_id"
   end
 
   create_table "organizations", force: :cascade do |t|
@@ -90,5 +92,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_05_125328) do
 
   add_foreign_key "attachments", "organizations"
   add_foreign_key "documents", "organizations"
+  add_foreign_key "documents", "spaces"
   add_foreign_key "spaces", "organizations"
 end
