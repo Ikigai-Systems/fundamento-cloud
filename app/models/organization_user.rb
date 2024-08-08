f6c16d82001521aa000276d6e3e2eca976623aeb
Pawel Niewiadomski <11110000b@gmail.com>
2024-08-08 13:56:25 +0200

    Update users table when user is added to the organization

--- app/models/organization_user.rb
+++ app/models/organization_user.rb
@@ -1,6 +1,26 @@
 class OrganizationUser < ApplicationRecord
   self.table_name = :organizations_users
+  self.primary_key = [:organization_id, :user_id]
 
   belongs_to :organization
   belongs_to :user
+
+  after_create_commit do
+    broadcast_prepend_to(
+      ["admin_users_list", self.organization],
+      target: "users",
+      partial: "admin/users/user",
+      locals: {
+        current_organization: self.organization,
+        user: self.user
+      }
+    )
+  end
+
+  after_destroy_commit do
+    broadcast_remove_to(
+      ["admin_users_list", self.organization],
+      target: self.user
+    )
+  end
 end
