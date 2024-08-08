8c619fe4946692e88c39abf17119b7c53c23c769
Pawel Niewiadomski <11110000b@gmail.com>
2024-08-08 13:34:44 +0200

    Track online presence per organization

--- app/channels/online_users_channel.rb
+++ app/channels/online_users_channel.rb
@@ -1,12 +1,12 @@
 class OnlineUsersChannel < ApplicationCable::Channel
 
   def subscribed
-    current_user&.change_to_online
+    current_user&.change_to_online(current_organization)
     super
   end
 
   def unsubscribed
-    current_user&.change_to_offline
+    current_user&.change_to_offline(current_organization)
     super
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+    :recoverable, :rememberable, :validatable, :trackable
 
   has_many :organizations_users, class_name: :OrganizationUser
   has_many :organizations, through: :organizations_users
@@ -15,30 +15,35 @@ class User < ApplicationRecord
 
   after_destroy_commit { broadcast_remove_to("online_users") }
 
-  def online?
-    Rails.cache.fetch("#{cache_key}/online", false)
-  end
-
   def initials
     first_name.first(1) + last_name.first(1)
   end
 
-  def last_online_at
-    Rails.cache.fetch("#{cache_key}/last_online_at", nil)
+  def online?(for_organization)
+    Rails.cache.fetch("#{for_organization.cache_key}/#{cache_key}/online", false)
   end
 
-  def change_to_online
-    Rails.cache.write("#{cache_key}/last_online_at", Time.now)
-    Rails.cache.write("#{cache_key}/online", true)
-
-    broadcast_replace_to("online_users")
-    broadcast_replace_to("admin_users_list", partial: "admin/users/user")
+  def last_online_at(for_organization)
+    Rails.cache.fetch("#{for_organization.cache_key}/#{cache_key}/last_online_at", nil)
   end
 
-  def change_to_offline
-    Rails.cache.write("#{cache_key}/online", false)
+  def change_to_online(for_organization)
+    Rails.cache.write("#{for_organization.cache_key}/#{cache_key}/last_online_at", Time.now)
+    Rails.cache.write("#{for_organization.cache_key}/#{cache_key}/online", true)
 
     broadcast_replace_to("online_users")
-    broadcast_replace_to("admin_users_list", partial: "admin/users/user")
+    broadcast_replace_to(["admin_users_list", for_organization],
+      partial: "admin/users/user",
+      locals: { current_organization: for_organization })
+  end
+
+  def change_to_offline(for_organization)
+    Rails.cache.write("#{for_organization.cache_key}/#{cache_key}/online", false)
+
+    broadcast_replace_to("online_users")
+    broadcast_replace_to(["admin_users_list", for_organization],
+      partial: "admin/users/user",
+      locals: { current_organization: for_organization }
+    )
   end
 end
