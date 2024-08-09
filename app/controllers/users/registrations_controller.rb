6e6430d005d19f1a32848a301f868bb3682fb820
Pawel Niewiadomski <11110000b@gmail.com>
2024-08-09 16:46:22 +0200

    Stay on the registration form after resource has been updated (instead of redirecting to root)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,4 +11,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
+
+  def after_update_path_for(resource)
+    sign_in_after_change_password? ? edit_registration_path(resource) : new_session_path(resource_name)
+  end
 end
