7d61ce63bea367fa3eb3387027e693530d09104a
Pawel Niewiadomski <11110000b@gmail.com>
2024-08-13 15:03:52 +0200

    Fixed problem with missing current_user

--- app/controllers/organizations_users_controller.rb
+++ app/controllers/organizations_users_controller.rb
@@ -7,6 +7,11 @@ class OrganizationsUsersController < ApplicationController
     organization = current_user.
       organizations.find(organization_id)
 
+    if current_user.id.to_s == user_id
+      redirect_to organization, notice: "You can't remove yourself from the organization."
+      return
+    end
+
     organization.organizations_users.
       find_by(organization_id: organization_id, user_id: user_id).
       destroy!
