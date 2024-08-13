59e66eae92a709f18b3265f8a9ab2f19ea2eb1d0
Pawel Niewiadomski <11110000b@gmail.com>
2024-08-13 14:04:03 +0200

    Additional settings for sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,6 +7,19 @@ if sentry_dsn.present?
     # get breadcrumbs from logs
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
+    # from https://docs.sentry.io/platforms/ruby/guides/rails/configuration/options/
+    config.backtrace_cleanup_callback = lambda do |backtrace|
+      Rails.backtrace_cleaner.clean(backtrace)
+    end
+
+    # this example uses Rails' parameter filter to sanitize the event payload
+    # for Rails 6+
+    filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+
+    config.before_send = lambda do |event, _hint|
+      filter.filter(event.to_hash)
+    end
+
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
