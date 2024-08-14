37e07017246a8f82b954045bfdcaf248a5aaafa1
Pawel Niewiadomski <11110000b@gmail.com>
2024-08-14 16:56:35 +0200

    Recently updated documents tab gets refreshed after every update

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,5 +2,20 @@ class Document < ApplicationRecord
   belongs_to :organization
   belongs_to :space
 
-  scope :recently_updated, -> { order(updated_at: :desc) }
+  scope :archived, -> { where(archived: true) }
+  scope :without_archived, -> { where(archived: false) }
+
+  scope :recently_updated, -> { without_archived.order(updated_at: :desc).limit(50) }
+
+  after_commit :broadcast_recently_updated_documents, on: [:create, :update, :destroy]
+
+  def broadcast_recently_updated_documents
+    broadcast_replace_to ["recent_documents_list", self.organization],
+      target: "recently_updated_documents_container",
+      partial: "documents/recently_updated_documents",
+      locals: {
+        recently_updated_documents: self.organization.documents.recently_updated,
+        current_organization: self.organization
+      }
+  end
 end
