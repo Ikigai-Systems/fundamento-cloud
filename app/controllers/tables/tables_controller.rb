6c852cd07e48c1ce13bb3239ad7f503ce1d91f38
Pawel Niewiadomski <11110000b@gmail.com>
2024-08-21 12:06:34 +0200

    Simplified index

--- app/controllers/tables/tables_controller.rb
+++ app/controllers/tables/tables_controller.rb
@@ -1,9 +1,5 @@
 class Tables::TablesController < ApplicationController
   def index
-    all_tables = current_organization.spaces.each_with_object([]) do |space, all_tables|
-      all_tables.concat(space.tables)
-    end
-
-    render json: all_tables
+    render json: current_organization.tables
   end
 end
