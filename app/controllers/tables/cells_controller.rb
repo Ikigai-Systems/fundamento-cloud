e28f0adf28279ebf404f4ae90582358f49f3e227
Pawel Niewiadomski <11110000b@gmail.com>
2024-08-21 15:18:02 +0200

    Basic controllers for rows, columns and cells

--- app/controllers/tables/cells_controller.rb
+++ app/controllers/tables/cells_controller.rb
@@ -6,9 +6,23 @@ class Tables::CellsController < ApplicationController
     render json: @row.cells
   end
 
+  def update
+    @cell = @row.find(params[:id])
+
+    if @cell.update(update_params)
+      render json: @cell.as_json(:except => [:organization_id, :table_id, :row_id, :column_id]), status: :ok
+    else
+      render json: @cell.errors, status: :unprocessable_content
+    end
+  end
+
   protected
 
   def load_table_row
     @row = self.current_organization.tables.find(params[:table_id]).rows.find(params[:row_id])
+  end
+
+  def update_params
+    params.require(:cell).permit(:value)
   end
 end
