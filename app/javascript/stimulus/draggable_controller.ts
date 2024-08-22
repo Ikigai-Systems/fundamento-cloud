dafb0ac2a92eaa63dfb80d9c72629f54c4087395
Piotr Stefaniak <stefan@ikigai.systems>
2024-08-22 17:49:15 +0200

    Hierarchy items can be nested by drag'n'dropping onto them

--- app/javascript/stimulus/draggable_controller.ts
+++ app/javascript/stimulus/draggable_controller.ts
@@ -19,37 +19,98 @@ export default class extends Controller<HTMLElement> {
       return accumulator;
     };
 
-    const fixPlaceholderDepth = (container: HTMLElement) => {
-      const placeholder: HTMLElement = container.querySelector('.sortable-placeholder');
+    const fixPlaceholder = () => {
+      const placeholder: HTMLElement = document.querySelector('.sortable-placeholder');
+      if (!placeholder) return;
+
+      const container = placeholder.closest("ul");
       const containerDepth = calculateDepth(container);
-      placeholder.style.marginLeft = `${16 * (containerDepth - 1)}px`;
-      placeholder.style.width = `${268 - (16 * (containerDepth - 1))}px`;
+      if (container.querySelectorAll("li:not(.sortable-placeholder)").length === 0) {
+        placeholder.style.height = "35px";
+        placeholder.style.top = "-13px";
+        placeholder.style.border = "solid 2px rgb(79 70 229)"; //bg-indigo-600
+        placeholder.style.borderRadius = "8px";
+        placeholder.style.backgroundColor = "inherit";
+        placeholder.dataset.surround = "true";
+        placeholder.style.marginLeft = "";
+        placeholder.style.width = "";
+      } else {
+        placeholder.style.marginLeft = `${16 * (containerDepth - 1)}px`;
+        placeholder.style.width = `${268 - (16 * (containerDepth - 1))}px`;
+        placeholder.dataset.surround = "false";
+        placeholder.style.height = "";
+        placeholder.style.top = "";
+        placeholder.style.border = "";
+        placeholder.style.borderRadius = "";
+        placeholder.style.backgroundColor = "";
+      }
     };
+
+    let fixPlaceholderIntervalId = undefined;
 
     sortable(this.itemTargets, {
       acceptFrom: '[data-draggable-target~="item"]',
-      placeholderClass: 'sortable-placeholder float-left bg-indigo-600 h-0.5 w-[268px] absolute before:absolute before:h-2 before:w-2 before:-top-[3px] before:border-2 before:border-solid before:rounded-full before:bg-white before:border-indigo-600'
+      placeholderClass: 'sortable-placeholder float-left bg-indigo-600 h-0.5 w-[268px] data-[surround=true]:before:hidden absolute before:absolute before:h-2 before:w-2 before:-top-[3px] before:border-2 before:border-solid before:rounded-full before:bg-white before:border-indigo-600'
     }).forEach((item: HTMLElement) => {
       item.addEventListener('sortupdate', async (e: CustomEvent) => {
         e.detail.item.querySelector(".document-padding-left").style.paddingLeft = `${16 * (calculateDepth(e.detail.destination.container) - 1)}px`;
 
+        const container = e.detail.destination.container;
+        container.classList.remove("h-[37px]", "mb-[-37px]", "top-[-37px]");
+
         const spaceId = this.element.dataset.spaceId;
-        const response = await SpacesApi.reorderHierarchy({params: {space_id: spaceId}, data: {
+        await SpacesApi.reorderHierarchy({params: {space_id: spaceId}, data: {
           id: spaceId,
           documentId: e.detail.item.querySelector("div[data-document-id]").dataset.documentId,
           parentId: item.dataset.documentId,
           position: e.detail.destination.index}});
       });
-      item.addEventListener('sortenter', (e: CustomEvent) => {
-        fixPlaceholderDepth(e.detail.destination.container);
-      });
       item.addEventListener('sortstart', (e: CustomEvent) => {
-        const intervalId = setInterval(() => {
-          if (e.detail.origin.container.querySelector('.sortable-placeholder')) {
-            fixPlaceholderDepth(e.detail.origin.container);
-            clearInterval(intervalId);
+        fixPlaceholderIntervalId = setInterval(() => {
+          fixPlaceholder();
+        }, 10)
+
+        setTimeout(() => {
+          this.itemTargets.forEach(container => {
+            // debugger;
+            if (container.children.length === 0) {
+              // element.style.backgroundColor = "red";
+              // element.style.opacity = "70%";
+              container.style.maxHeight = "inherit";
+              container.style.height = "19px";
+              container.style.position = "relative";
+              container.style.top = "-23px";
+              container.style.marginBottom = "-19px";
+              container.style.overflow = "visible";
+            }
+          });
+        }, 10)
+      });
+      item.addEventListener('sortstop', (e: CustomEvent) => {
+        clearInterval(fixPlaceholderIntervalId);
+
+        this.itemTargets.forEach(container => {
+          const li = container.closest("li");
+          if (li) {
+            const expanderIcon = li.querySelector(".multi-items-expander");
+            const dotIcon = li.querySelector(".single-item-dot");
+            if (container.children.length >= 1) {
+              expanderIcon.classList.remove("hidden");
+              dotIcon.classList.add("hidden");
+            } else {
+              expanderIcon.classList.add("hidden");
+              dotIcon.classList.remove("hidden");
+            }
           }
-        }, 10);
+          container.style.backgroundColor = "";
+          container.style.maxHeight = "";
+          container.style.height = "";
+          container.style.position = "";
+          container.style.top = "";
+          container.style.marginBottom = "";
+          container.style.opacity = "";
+          container.style.overflow = "";
+        });
       });
     });
   }
