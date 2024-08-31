d2169d933edbd08070b0e430e9b1bf3c140ef0f4
Pawel Niewiadomski <11110000b@gmail.com>
2024-08-31 18:24:49 +0200

    Basic public page for document

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,18 @@
 class PublicController < ApplicationController
-  def show
+  skip_before_action :authenticate_user!
+  skip_before_action :ensure_organization_exists
+  skip_before_action :select_current_organization
+  skip_before_action :load_current_organization_from_cookie
+  skip_before_action :ensure_space_exists
 
+  def show
+    @public_link = PublicLink.find_by_npi!(params[:npi])
+    @object = @public_link.object
+
+    if @object.is_a?(Document)
+      render "document", locals: { document: @object }
+    else
+      render status: :unprocessable_content
+    end
   end
 end
