cd2a33d82f37f0333c007a9a763b01f108c78218
Piotr Stefaniak <stefan@ikigai.systems>
2024-09-10 16:51:34 +0200

    Adjust version_controller to use space_npi instead of space_id

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -13,7 +13,9 @@ class VersionsController < ApplicationController
       Sentry.capture_message("XmlfragmentToBlock mismatch for #{params[:space_id]} / #{params[:document_id]} (call stefan) : #{diff}")
     end
 
-    redirect_to edit_space_document_path(params[:space_id], params[:document_id])
+    # pp JSON.pretty_generate(blocks2)
+
+    redirect_to edit_space_document_path(params[:space_npi], params[:document_id])
 
     # respond_to do |format|
     #   format.json { render json: current_organization.documents, :except => [:sync] }
