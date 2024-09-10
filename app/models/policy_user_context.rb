f19b67af9104642b0afe70b8fe8350b2491576e6
Pawel Niewiadomski <11110000b@gmail.com>
2024-09-10 13:05:32 +0200

    Added organization_user to PolicyUserContext

--- app/models/policy_user_context.rb
+++ app/models/policy_user_context.rb
@@ -1,8 +1,9 @@
 class PolicyUserContext
-  attr_reader :user, :current_organization
+  attr_reader :user, :current_organization, :organization_user
 
   def initialize(user, current_organization)
     @user = user
     @current_organization = current_organization
+    @organization_user = OrganizationUser.find_by(organization: current_organization, user: user)
   end
 end
