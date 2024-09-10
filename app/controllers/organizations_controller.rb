4f2d678df51865512e5247f73656ce1aff91faa7
Pawel Niewiadomski <11110000b@gmail.com>
2024-09-10 13:06:11 +0200

    Added OrganizationPolicy and updated OrganizationsController to use it

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -4,16 +4,24 @@ class OrganizationsController < ApplicationController
   skip_before_action :ensure_organization_exists, only: [:new, :create]
   skip_before_action :select_current_organization
 
+  after_action :verify_authorized
+
   def new
     @organization = Organization.new
+
+    authorize @organization, :new?
   end
 
   def index
     @organizations = current_user.organizations.order(:name)
+
+    authorize @organizations, :index?
   end
 
   def create
     @organization = current_user.organizations.new(organization_params)
+
+    authorize @organization, :create?
 
     if @organization.save
       OrganizationUser.create!(
@@ -30,6 +38,8 @@ class OrganizationsController < ApplicationController
   def update
     @organization = current_user.organizations.find_by_npi!(params[:npi])
 
+    authorize @organization, :update?
+
     if @organization.update(organization_params)
       redirect_to @organization, notice: 'Organization was successfully updated.'
     else
@@ -39,14 +49,20 @@ class OrganizationsController < ApplicationController
 
   def show
     @organization = current_user.organizations.find_by_npi!(params[:npi])
+
+    authorize @organization, :show?
   end
 
   def edit
     @organization = current_user.organizations.find_by_npi!(params[:npi])
+
+    authorize @organization, :edit?
   end
 
   def select
     @organization = current_user.organizations.find_by_npi!(params[:npi])
+
+    authorize @organization, :select?
 
     cookies.encrypted[:organization_id] = @organization.id
 
@@ -55,6 +71,9 @@ class OrganizationsController < ApplicationController
 
   def destroy
     @organization = current_user.organizations.find_by_npi!(params[:npi])
+
+    authorize @organization, :destroy?
+
     @organization.destroy!
 
     redirect_to organizations_path, notice: "Organization was removed."
---
+++ app/policies/organization_policy.rb
@@ -0,0 +1,26 @@
+class OrganizationPolicy < ApplicationPolicy
+  def show?
+    # Everyone can show an organization it belongs to
+    true
+  end
+
+  def index?
+    show?
+  end
+
+  def update?
+    organization_user.manager?
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    update?
+  end
+
+  def select?
+    show?
+  end
+end
