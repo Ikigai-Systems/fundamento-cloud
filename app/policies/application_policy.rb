d4329803d09a16d6f13a50ab0a813a91d8d094ab
Pawel Niewiadomski <11110000b@gmail.com>
2024-09-10 13:04:59 +0200

    Added ApplicationPolicy

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+class ApplicationPolicy
+  attr_reader :user_context, :record
+
+  delegate :user, to: :user_context
+  delegate :current_organization, to: :user_context
+  delegate :organization_user, to: :user_context
+
+  def initialize(user_context, record)
+    @user_context = user_context
+    @record = record
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  class Scope
+    def initialize(user_context, scope)
+      @user_context = user_context
+      @scope = scope
+    end
+
+    def resolve
+      raise NoMethodError, "You must define #resolve in #{self.class}"
+    end
+
+    private
+
+    attr_reader :user_context, :scope
+  end
+end
