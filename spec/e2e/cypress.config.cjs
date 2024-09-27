2cf2b7e716be3dbbdd65e09737245f102b38a72c
Pawel Niewiadomski <11110000b@gmail.com>
2024-09-27 16:08:20 +0200

    Changed where cypress will store videos and screenshots

--- spec/e2e/cypress.config.cjs
+++ spec/e2e/cypress.config.cjs
@@ -2,8 +2,12 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5017",
+    baseUrl: "http://localhost:3000",
     defaultCommandTimeout: 10000,
     supportFile: "cypress/support/index.js",
-  }
+  },
+  trashAssetsBeforeRuns: true,
+  downloadsFolder: "../../log/cypress/downloads",
+  screenshotsFolder: "../../log/cypress/screenshots",
+  videosFolder: "../../log/cypress/videos",
 })
