636105c2e2e6bcaa5922b4f4cd924c059c3e2bc3
Pawel Niewiadomski <11110000b@gmail.com>
2024-09-28 21:23:37 +0200

    Ignore unhandled promises

--- spec/e2e/cypress/support/index.js
+++ spec/e2e/cypress/support/index.js
@@ -18,5 +18,18 @@
 import './commands'
 import './on-rails'
 
+Cypress.on('uncaught:exception', (err, runnable, promise) => {
+  // when the exception originated from an unhandled promise
+  // rejection, the promise is provided as a third argument
+  // you can turn off failing the test in this case
+  if (promise) {
+    // debugger;
+
+    return false
+  }
+  // we still want to ensure there are no other unexpected
+  // errors, so we let them fail the test
+})
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
