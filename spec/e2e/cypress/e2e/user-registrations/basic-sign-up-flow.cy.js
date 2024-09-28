04d22453701e0d71cfb44866cab0136bc17fbe72
Pawel Niewiadomski <11110000b@gmail.com>
2024-09-28 19:10:58 +0200

    Additional E2E tests

--- spec/e2e/cypress/e2e/user-registrations/basic-sign-up-flow.cy.js
+++ spec/e2e/cypress/e2e/user-registrations/basic-sign-up-flow.cy.js
@@ -1,9 +1,7 @@
-describe('Basic sign-up flow works', function() {
-  beforeEach(() => {
+describe('Basic features work', function() {
+  before(() => {
     cy.app('clean') // have a look at e2e/app_commands/clean.rb
-  })
 
-  it('signing-up', function() {
     cy.visit("/");
     cy.contains("Sign up").click();
     cy.get('input[name="user[first_name]"]').type("Pawel");
@@ -12,7 +10,49 @@ describe('Basic sign-up flow works', function() {
     cy.get('input[name="user[password]"]').type("password");
     cy.get('input[name="user[password_confirmation]"]').type("password");
     cy.get('input[type=submit]').click();
-    cy.contains('Create organization').should('be.visible');
+
+    cy.contains('Create organization');
+
+    cy.get('input[name="organization[name]"]').type("E2E Tests");
+    cy.get('input[type=submit]').click();
+
+    cy.contains("Organization was successfully created");
+
+    cy.get("body nav").contains("PN").click();
+    cy.contains("Sign out").click();
+
+    cy.url().should('include', '/users/sign_in');
+  });
+
+  beforeEach(() => {
+    cy.visit("/");
+
+    cy.get('input[name="user[email]"]').type("pawel.nowak@random.pl");
+    cy.get('input[name="user[password]"]').type("password");
+    cy.get('input[type=submit]').click();
+  });
+
+  it("create new organization", function() {
+    cy.visit("/organizations/new");
+
+    cy.get('input[name="organization[name]"]').type("Second organization");
+    cy.get('input[type=submit]').click();
+
+    cy.contains("Organization was successfully created");
+
+    cy.contains("Switch to").click();
+
+    cy.contains("Second organization Space");
   })
+
+  it("you can create teams", function() {
+    cy.visit("/");
+
+    cy.get("body nav").contains("PN").click();
+
+    cy.contains("Teams").click();
+
+    cy.get(".focused-header").contains("Create").click();
+  });
 })
 
