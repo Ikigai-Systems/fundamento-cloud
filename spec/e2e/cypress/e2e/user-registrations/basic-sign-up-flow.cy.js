3ab1e3ca42250ae31b7f8954a7623c7fcf5f13d0
Pawel Niewiadomski <11110000b@gmail.com>
2024-09-28 07:40:46 +0200

    Fixed race condition in basic sign up test

--- spec/e2e/cypress/e2e/user-registrations/basic-sign-up-flow.cy.js
+++ spec/e2e/cypress/e2e/user-registrations/basic-sign-up-flow.cy.js
@@ -6,9 +6,9 @@ describe('Basic sign-up flow works', function() {
   it('signing-up', function() {
     cy.visit("/");
     cy.contains("Sign up").click();
-    cy.get('input[name="user[email]"]').type("pawel.nowak@random.pl");
     cy.get('input[name="user[first_name]"]').type("Pawel");
     cy.get('input[name="user[last_name]"]').type("Nowak");
+    cy.get('input[name="user[email]"]').type("pawel.nowak@random.pl");
     cy.get('input[name="user[password]"]').type("password");
     cy.get('input[name="user[password_confirmation]"]').type("password");
     cy.get('input[type=submit]').click();
