3ffecdc07008406fccbc21e60f53bd2335110c60
Pawel Niewiadomski <11110000b@gmail.com>
2024-10-24 21:23:22 +0200

    Defined healthcheck from minio

--- bin/minio-init.sh
+++ bin/minio-init.sh
@@ -2,8 +2,6 @@
 
 set -xe
 
-sleep 5
-
 mc alias set minio http://${MINIO_HOST}:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 mc mb minio/fundamento-development
 mc admin user add minio fundamento IPaWkaUi9Ko1NA
