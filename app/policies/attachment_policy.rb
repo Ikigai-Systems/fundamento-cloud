5a0f0eb383f575474ef8e0bd6ca6b9088485717b
Pawel Niewiadomski <11110000b@gmail.com>
2024-10-28 21:59:08 +0100

    Don't use Pundit.authorize in policies as this throws an exception, switching to Pundit.policy

--- app/policies/attachment_policy.rb
+++ app/policies/attachment_policy.rb
@@ -1,17 +1,17 @@
 class AttachmentPolicy < ApplicationPolicy
   def show?
-    Pundit.authorize user_context, record.parent, :show?
+    Pundit.policy!(user_context, record.parent).show?
   end
 
   def create?
-    Pundit.authorize user_context, record.parent, :update?
+    Pundit.policy!(user_context, record.parent).update?
   end
 
   def update?
-    Pundit.authorize user_context, record.parent, :update?
+    Pundit.policy!(user_context, record.parent).update?
   end
 
   def destroy?
-    Pundit.authorize user_context, record.parent, :update?
+    Pundit.policy!(user_context, record.parent).update?
   end
 end
--- app/policies/document_policy.rb
+++ app/policies/document_policy.rb
@@ -6,18 +6,18 @@ class DocumentPolicy < ApplicationPolicy
   end
 
   def show?
-    Pundit.authorize user_context, record.space, :show?
+    Pundit.policy!(user_context, record.space).show?
   end
 
   def create?
-    Pundit.authorize user_context, record.space, :update?
+    Pundit.policy!(user_context, record.space).update?
   end
 
   def update?
-    Pundit.authorize user_context, record.space, :update?
+    Pundit.policy!(user_context, record.space).update?
   end
 
   def destroy?
-    Pundit.authorize user_context, record.space, :update?
+    Pundit.policy!(user_context, record.space).update?
   end
 end
