059889f5c0ddd530a05fd25150ae129cf94a05df
Pawel Niewiadomski <11110000b@gmail.com>
2024-11-02 17:35:20 +0100

    Favorites changes are broadcasted to the list in the root view

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -10,4 +10,6 @@ class Favorite < ApplicationRecord
   validates :object_type, inclusion: { in: %w[Document Table] }
 
   validates_uniqueness_of :object_id, scope: [:organization_user_id, :object_type]
+
+  broadcasts_to -> (favorite) { [ favorite.organization_user, :favorites ] }, inserts_by: :prepend, target: "favorites_list"
 end
