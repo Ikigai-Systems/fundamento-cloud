ad851c6e6625719b0a010de5e8f2903967f459fc
Pawel Niewiadomski <11110000b@gmail.com>
2024-11-08 19:48:08 +0100

    Show table page shows a read-only view of the table

---
+++ app/javascript/components/ViewTablePanel.tsx
@@ -0,0 +1,18 @@
+import EditableTableWithRowstack from "./tables/EditableTableWithRowstack.tsx";
+import CurrentSpaceContext from "../contextes/CurrentSpaceContext.tsx";
+import {TableData} from "tables/EditableTableWithRowstack.tsx"
+import {Space, Table} from "../types.ts"
+
+const ViewTablePanel = ({table, data, space}: ViewTablePanelProps) => {
+  return <CurrentSpaceContext.Provider value={{space}}>
+    <EditableTableWithRowstack table={table} data={data} isEditable={false}/>
+  </CurrentSpaceContext.Provider>
+}
+
+type ViewTablePanelProps = {
+  table: Table,
+  space: Space,
+  data: TableData
+}
+
+export default ViewTablePanel;
