8f87bb17180e6e66a52a84060788199f677b3891
Pawel Niewiadomski <11110000b@gmail.com>
2024-11-09 19:54:27 +0100

    Reverted me accidentally disabling create buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,7 @@
 module ApplicationHelper
   def link_to_with_disable(name = nil, options = nil, html_options = nil, &block)
-    html_options, options, name = options, name, block if block_given?
-
-    if html_options.is_a?(Hash) && html_options[:disabled]
-      content_tag(:span, name, html_options, &block)
+    if (block_given? ? options : html_options).is_a?(Hash) && (block_given? ? options : html_options)[:disabled]
+      content_tag(:span, name, (block_given? ? options : html_options), &block)
     else
       link_to(name, options, html_options, &block)
     end
