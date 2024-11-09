207bb1cea4a3e93c48dae767168e6fe3ea3aa2c6
Pawel Niewiadomski <11110000b@gmail.com>
2024-11-09 19:46:38 +0100

    Create table link will be disabled if you can't modify the space

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
   def link_to_with_disable(name = nil, options = nil, html_options = nil, &block)
+    html_options, options, name = options, name, block if block_given?
+
     if html_options.is_a?(Hash) && html_options[:disabled]
-      content_tag(:span, name, html_options)
+      content_tag(:span, name, html_options, &block)
     else
       link_to(name, options, html_options, &block)
     end
