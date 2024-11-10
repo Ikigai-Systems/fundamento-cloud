117d78cd2429be3a1538f0973b878fd67e914ff2
Pawel Niewiadomski <11110000b@gmail.com>
2024-11-10 14:48:26 +0100

    Added TablePolicy

---
+++ app/policies/table_policy.rb
@@ -0,0 +1,23 @@
+class TablePolicy < ApplicationPolicy
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      scope.where(space: Pundit.policy_scope!(user_context, Space))
+    end
+  end
+
+  def show?
+    Pundit.policy!(user_context, record.space).show?
+  end
+
+  def create?
+    Pundit.policy!(user_context, record.space).update?
+  end
+
+  def update?
+    Pundit.policy!(user_context, record.space).update?
+  end
+
+  def destroy?
+    Pundit.policy!(user_context, record.space).update?
+  end
+end
