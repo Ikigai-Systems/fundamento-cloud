7d6eb8bffa2f72c202c7a6ec312197fb7bcb66aa
Piotr Stefaniak <stefan@ikigai.systems>
2024-11-15 18:38:52 +0100

    Proof of concept micro service for evaluating formulas. Currently doing round-trip back to rails app.

---
+++ formula-eval/src/server.js
@@ -0,0 +1,34 @@
+import Fastify from 'fastify';
+import FormBodyPlugin from '@fastify/formbody'
+
+const fastify = Fastify({logger: true});
+fastify.register(FormBodyPlugin);
+
+// Declare a route
+fastify.post('/formulas/eval', async function handler (request, reply) {
+  // todo: some kind of authentication for veryfing this is legitimate request from fundamento app
+
+  const {formula, additional_context} = request.body;
+
+  // simulate formula evaluation via round-trip to fundamento formulas_controller.rb:
+  const fundamentoFormulasEvalUrl = process.env.FUNDAMENTO_FORMULAS_EVAL_URL || "http://localhost:3000/formulas/eval"
+  const response = await fetch(fundamentoFormulasEvalUrl, {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      'formula': formula,
+      'additional_context': additional_context,
+    })
+  })
+  return await response.json();
+})
+
+try {
+  await fastify.listen({ port: process.env.SERVER_PORT || 3001 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
