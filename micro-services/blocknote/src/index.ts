f7bcd5b460e09473d0d8a607914042d407ddaa44
Pawel Niewiadomski <11110000b@gmail.com>
2024-11-19 13:55:21 +0100

    Extract convertToBlocks to a separate file

--- micro-services/blocknote/src/index.ts
+++ micro-services/blocknote/src/index.ts
@@ -1,23 +1,5 @@
-import { ServerBlockNoteEditor } from "@blocknote/server-util";
-import * as Y from 'yjs';
 import fs from "fs";
-import schema from "../../../app/javascript/components/editor/schema";
-
-function convertToBlocks(base64: string) {
-  const update = Uint8Array.from(Buffer.from(base64, 'base64'));
-  const doc = new Y.Doc();
-  Y.applyUpdate(doc, update);
-
-  const serverBlockNoteEditor = ServerBlockNoteEditor.create({
-    schema,
-  });
-
-  if (doc.getXmlFragment("document-store").length !== 0) {
-    return serverBlockNoteEditor.yDocToBlocks(doc, "document-store");
-  } else {
-    return [];
-  }
-}
+import {convertToBlocks} from "./convertToBlocks";
 
 async function readStdin() {
   const chunks = [];
