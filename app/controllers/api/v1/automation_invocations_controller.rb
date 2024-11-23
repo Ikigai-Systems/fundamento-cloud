c1d50c0ba844ba5371dadf56d94e6f074577a9e3
Pawel Niewiadomski <11110000b@gmail.com>
2024-11-23 23:11:19 +0100

    Additional logs in automation_invocations_controller.rb

--- app/controllers/api/v1/automation_invocations_controller.rb
+++ app/controllers/api/v1/automation_invocations_controller.rb
@@ -2,23 +2,44 @@ class Api::V1::AutomationInvocationsController < Api::ApiController
   before_action :load_automation
 
   def create
-    unless @automation.webhook?
-      render json: nil, status: :precondition_failed
-      return
+    logger.tagged(@automation.npi) do
+      unless @automation.webhook?
+        render json: nil, status: :precondition_failed
+        return
+      end
+
+      if @automation.debug_at.present?
+        logger.info("Received webhook automation request")
+
+        # Check for the Authorization header
+        auth_header = request.headers['Authorization']
+        if auth_header.present?
+          last_four = auth_header[-4..-1]
+          logger.info "Authorization header present. Last 4 characters: #{last_four}"
+        else
+          logger.info "Authorization header not present."
+        end
+
+        logger.info "Request Content-Type: #{request.content_type}"
+        logger.info "Request Body: #{request.raw_post}"
+        logger.info "Request Method: #{request.request_method}"
+        logger.info "User Agent: #{request.user_agent}"
+      end
+
+
+      # If we ever get to a point this gives us memory bloat refer to CLK webhooks processing for a memory optimized version
+      invocation = @automation.invocations.create!(
+        organization_id: @automation.organization_id,
+        space_id: @automation.space_id,
+        kind: @automation.kind,
+        formula: @automation.formula,
+        webhook: request.raw_post
+      )
+
+      AutomationInvocationJob.perform_later(invocation)
+
+      render json: { id: invocation.id }, status: :created
     end
-
-    # If we ever get to a point this gives us memory bloat refer to CLK webhooks processing for a memory optimized version
-    invocation = @automation.invocations.create!(
-      organization_id: @automation.organization_id,
-      space_id: @automation.space_id,
-      kind: @automation.kind,
-      formula: @automation.formula,
-      webhook: request.raw_post
-    )
-
-    AutomationInvocationJob.perform_later(invocation)
-
-    render json: { id: invocation.id }, status: :created
   end
 
   private
