cbed493e257725ee3eed15858b96092ca2642b3e
Pawel Niewiadomski <11110000b@gmail.com>
2024-11-23 16:04:09 +0100

    AutomationInvocationJob tries to evaluate the formula

--- app/jobs/automation_invocation_job.rb
+++ app/jobs/automation_invocation_job.rb
@@ -1,5 +1,15 @@
 class AutomationInvocationJob < ApplicationJob
   def perform(automation_invocation)
-    automation_invocation.update!(invoked_at: Time.now)
+    invoked_at = Time.now
+
+    additional_context = JSON.parse(automation_invocation.webhook)
+
+    result = FormulaEvalGateway.evaluate(automation_invocation.formula, additional_context)
+
+    automation_invocation.update!(invoked_at: invoked_at, result: result.to_json)
+  rescue => e
+    automation_invocation.update!(invoked_at: invoked_at, result: {
+      error: e.to_s
+    }.to_json)
   end
 end
