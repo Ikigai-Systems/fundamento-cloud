1df48a229b59f092cd10ea52da5d5bc920912d44
Piotr Stefaniak <stefan@ikigai.systems>
2024-11-27 16:14:50 +0100

    Making Button configuration appear where there is space in viewport

--- app/javascript/components/editor/inline-content/ButtonInlineContent.tsx
+++ app/javascript/components/editor/inline-content/ButtonInlineContent.tsx
@@ -1,5 +1,5 @@
 import {createReactInlineContentSpec, useBlockNoteEditor} from "@blocknote/react";
-import {autoUpdate, useFloating} from '@floating-ui/react';
+import {autoUpdate, useFloating, useDismiss, useInteractions, FloatingPortal, flip} from '@floating-ui/react';
 import {useState} from "react";
 import EditButtonPopup from "../../tables/rowstack/EditButtonPopup.tsx";
 
@@ -19,17 +19,25 @@ const ButtonInlineContent = createReactInlineContentSpec(
   {
     /* eslint-disable react-hooks/rules-of-hooks */
     render: (props) => {
-      const blockProps = props.inlineContent.props;
+      const inlineContentProps = props.inlineContent.props;
       const {isEditable} = useBlockNoteEditor();
       const [isConfigurationOpen, setIsConfigurationOpen] = useState(false);
-      const {refs, floatingStyles} = useFloating({
+      const {refs, floatingStyles, context} = useFloating({
         whileElementsMounted: autoUpdate,
         open: isConfigurationOpen,
         onOpenChange: setIsConfigurationOpen,
+        middleware: [flip()],
+        placement: "bottom-start",
       });
 
+      const dismiss = useDismiss(context);
+
+      const {getReferenceProps, getFloatingProps} = useInteractions([
+        dismiss,
+      ]);
+
       return (<>
-        <span ref={refs.setReference} className="inline-flex flex-row items-center group">
+        <span ref={refs.setReference} {...getReferenceProps()} className="inline-flex flex-row items-center group">
           <button className="secondary-button z-10">
             Button
           </button>
@@ -41,9 +49,16 @@ const ButtonInlineContent = createReactInlineContentSpec(
           >
             <span className="group-hover:opacity-30 opacity-0 size-7 icon-[heroicons--cog-6-tooth]"></span>
           </button>}
-          {isConfigurationOpen && <div ref={refs.setFloating} style={floatingStyles} className="z-[1001] bg-neutral-100">
-            <EditButtonPopup/>
-          </div>}
+          {isConfigurationOpen && <FloatingPortal>
+            <div
+              ref={refs.setFloating}
+              style={floatingStyles}
+              {...getFloatingProps()}
+              className="bg-neutral-100 z-10"
+            >
+              <EditButtonPopup/>
+            </div>
+          </FloatingPortal>}
         </span>
       </>);
     },
