388a8f57474939894d9480227f06f2b8c306d305
Piotr Stefaniak <stefan@ikigai.systems>
2024-11-27 01:34:57 +0100

    Sample button configuration popup (placeholder)

--- app/javascript/components/editor/inline-content/ButtonInlineContent.tsx
+++ app/javascript/components/editor/inline-content/ButtonInlineContent.tsx
@@ -1,4 +1,7 @@
-import {createReactBlockSpec, createReactInlineContentSpec, useBlockNoteEditor} from "@blocknote/react";
+import {createReactInlineContentSpec, useBlockNoteEditor} from "@blocknote/react";
+import {autoUpdate, useFloating} from '@floating-ui/react';
+import {useState} from "react";
+import EditButtonPopup from "../../tables/rowstack/EditButtonPopup.tsx";
 
 const ButtonInlineContent = createReactInlineContentSpec(
   {
@@ -17,16 +20,29 @@ const ButtonInlineContent = createReactInlineContentSpec(
     /* eslint-disable react-hooks/rules-of-hooks */
     render: (props) => {
       const blockProps = props.inlineContent.props;
-
       const {isEditable} = useBlockNoteEditor();
+      const [isConfigurationOpen, setIsConfigurationOpen] = useState(false);
+      const {refs, floatingStyles} = useFloating({
+        whileElementsMounted: autoUpdate,
+        open: isConfigurationOpen,
+        onOpenChange: setIsConfigurationOpen,
+      });
 
       return (<>
-        <span className="inline-flex flex-row items-center group">
+        <span ref={refs.setReference} className="inline-flex flex-row items-center group">
           <button className="secondary-button z-10">
             Button
           </button>
-          {isEditable && <div className="flex flex-row items-center secondary-button pl-2.5 py-1 pr-1 -ml-2">
+          {isEditable && <button
+            className="flex flex-row items-center secondary-button pl-2.5 py-1 pr-1 -ml-2"
+            onClick={() => {
+              setIsConfigurationOpen(!isConfigurationOpen);
+            }}
+          >
             <span className="group-hover:opacity-30 opacity-0 size-7 icon-[heroicons--cog-6-tooth]"></span>
+          </button>}
+          {isConfigurationOpen && <div ref={refs.setFloating} style={floatingStyles} className="z-[1001] bg-neutral-100">
+            <EditButtonPopup/>
           </div>}
         </span>
       </>);
