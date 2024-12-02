2c02562fc466e237ef3772ebd433af2c373dd687
Pawel Niewiadomski <11110000b@gmail.com>
2024-12-02 18:28:05 +0100

    Moved code running the formula to AutomationInvocation so you can run it synchronously from the console

--- app/jobs/automation_invocation_job.rb
+++ app/jobs/automation_invocation_job.rb
@@ -1,15 +1,5 @@
 class AutomationInvocationJob < ApplicationJob
   def perform(automation_invocation)
-    invoked_at = Time.now
-
-    additional_context = JSON.parse(automation_invocation.webhook)
-
-    result = FormulaEvalGateway.evaluate(automation_invocation.formula, additional_context)
-
-    automation_invocation.update!(invoked_at: invoked_at, result: result.to_json)
-  rescue => e
-    automation_invocation.update!(invoked_at: invoked_at, result: {
-      error: e.to_s
-    }.to_json)
+    automation_invocation.invoke!
   end
 end
--- app/models/automation_invocation.rb
+++ app/models/automation_invocation.rb
@@ -8,4 +8,18 @@ class AutomationInvocation < ApplicationRecord
   scope :recently_invoked, -> { order(invoked_at: :desc) }
 
   broadcasts_to -> (invocation) { [ invocation.space, invocation.automation, :invocations ] }, inserts_by: :prepend, partial: "automations/invocation"
+
+  def invoke!
+    invoked_at = Time.now
+
+    additional_context = JSON.parse(self.webhook)
+
+    result = FormulaEvalGateway.evaluate(self.formula, additional_context)
+
+    self.update!(invoked_at: invoked_at, result: result.to_json)
+  rescue => e
+    self.update!(invoked_at: invoked_at, result: {
+      error: e.to_s
+    }.to_json)
+  end
 end
