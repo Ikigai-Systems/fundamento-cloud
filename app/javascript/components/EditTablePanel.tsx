d47d091021a28d28be6c9d3974ca7db498323621
Piotr Stefaniak <stefan@ikigai.systems>
2024-12-05 00:51:58 +0100

    Table will re-render upon editing formula in TableEditPanel

--- app/javascript/components/EditTablePanel.tsx
+++ app/javascript/components/EditTablePanel.tsx
@@ -3,8 +3,30 @@ import CurrentSpaceContext from "../contextes/CurrentSpaceContext.tsx";
 import {TableData} from "tables/EditableTableWithRowstack.tsx"
 import {Space, Table} from "../types.ts"
 import {TableTitleInput} from "./ContentTitle.tsx";
+import {useQuery} from "@tanstack/react-query";
+import {Config} from "@js-from-routes/client";
+import TablesApi from "../api/Tables/TablesApi.js";
+import queryClient from "../contextes/ReactQueryClient.tsx";
+import {useRef} from "react";
 
 const EditTablePanel = ({table, data, space}: EditTablePanelProps) => {
+  const initialRender = useRef(true);
+
+  const tableQuery = useQuery({queryKey: ["tables", space?.npi, table.id], queryFn: async () => {
+    if (initialRender.current === true) {
+      initialRender.current = false;
+      return {table, data, forceReRenderUuid: crypto.randomUUID()}
+    } else {
+      const currentDataDeserializer = Config.deserializeData;
+      Config.deserializeData = (val => val);
+      const promiseData = TablesApi.show({space_npi: space?.npi, id: table.id});
+      Config.deserializeData = currentDataDeserializer;
+      const data = await promiseData
+      return {...data, forceRerenderUuid: crypto.randomUUID()}
+    }
+  }}, queryClient);
+  const {isLoading, isError} = tableQuery;
+
   return <CurrentSpaceContext.Provider value={{space}}>
 
     <div className="flex flex-col">
@@ -13,7 +35,7 @@ const EditTablePanel = ({table, data, space}: EditTablePanelProps) => {
       </div>
 
       <div className="content-editor-padding">
-        <EditableTableWithRowstack table={table} data={data}/>
+        {!isLoading && !isError && <EditableTableWithRowstack table={tableQuery.data.table} data={tableQuery.data.data} forceRerenderUuid={tableQuery.data.forceRerenderUuid}/>}
       </div>
     </div>
   </CurrentSpaceContext.Provider>
