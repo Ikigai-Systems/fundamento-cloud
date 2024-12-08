d47683f19e4274f3e0a11eda52c55c4413bca5a8
Pawel Niewiadomski <11110000b@gmail.com>
2024-12-08 18:53:25 +0100

    Updated production.rb DNS rebinding protection [skip-tests]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,10 +101,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    "fundamento.cloud",     # Allow requests from example.com
+    /.*\.fundamento\.cloud/, # Allow requests from subdomains like `www.example.com`
+    # Allow internal requests in Kubernetes cluster
+    "website",
+    "website.fundamento-cloud.svc.cluster.local",
+  ]
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
