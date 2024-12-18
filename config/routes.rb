180fe5f8844e0b12dfc4cc3155d739d422b9c6b1
Pawel Niewiadomski <11110000b@gmail.com>
2024-12-18 10:23:26 +0100

    Added flipper ui under /construction/flipper

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,10 @@ Rails.application.routes.draw do
     mount GoodJob::Engine => "/construction/jobs"
   end
 
+  authenticate :superintendent do
+    mount Flipper::UI.app(Flipper) => "/construction/flipper"
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
