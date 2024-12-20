be08d977bf5f02969d57f5e659e53d8d45751d31
Pawel Niewiadomski <11110000b@gmail.com>
2024-12-20 20:25:39 +0100

    Refactor space_spec.rb to make hierarchy reusable

--- spec/models/space_spec.rb
+++ spec/models/space_spec.rb
@@ -1,18 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Space, type: :model do
+  let(:hierarchy) do
+    [
+      { "id" => 16, "children" => [] },
+      { "id" => 18, "children" => [] },
+      { "id" => 23, "children" => [{ "id" => 24, "children" => [
+        { "id" => 26, "children" => [] }
+      ] }] },
+      { "id" => 19, "children" => [{ "id" => 20, "children" => [] }] },
+      { "id" => 22, "children" => [{ "id" => 25, "children" => [] }] }
+    ]
+  end
+
+  let(:hierarchy_1) do
+    [
+      { "id" => 16, "children" => [] },
+      { "id" => 18, "children" => [] },
+      { "id" => 23, "children" => [{ "id" => 24, "children" => [
+        { "id" => 26, "children" => [] }
+      ] }] },
+      { "id" => 19, "children" => [{ "id" => 20, "children" => [] }] },
+      { "id" => 22, "children" => [{ "id" => 25, "children" => [] }] }
+    ]
+  end
+
+  let(:hierarchy_2) do
+    [
+      { "id" => 16, "children" => [] },
+      { "id" => 18, "children" => [] },
+      { "id" => 23, "children" => [{ "id" => 24, "children" => [
+        { "id" => 26, "children" => [] }
+      ] }] },
+      { "id" => 19, "children" => [{ "id" => 20, "children" => [] }] },
+      { "id" => 22, "children" => [{ "id" => 25, "children" => [] }] }
+    ]
+  end
+
+  let(:hierarchy_3) do
+    [
+      { "id" => 16, "children" => [] },
+      { "id" => 18, "children" => [] },
+      { "id" => 23, "children" => [{ "id" => 24, "children" => [
+        { "id" => 26, "children" => [] }
+      ] }] },
+      { "id" => 19, "children" => [{ "id" => 20, "children" => [] }] },
+      { "id" => 22, "children" => [{ "id" => 25, "children" => [] }] }
+    ]
+  end
+
   describe "#remove_single_item_from_hierarchy!" do
     it "removes only one item and moves children level up" do
-      hierarchy = [
-        { "id" => 16, "children" => [] },
-        { "id" => 18, "children" => [] },
-        { "id" => 23, "children" => [{ "id" => 24, "children" => [
-          { "id" => 26, "children" => [] }
-        ] }] },
-        { "id" => 19, "children" => [{ "id" => 20, "children" => [] }] },
-        { "id" => 22, "children" => [{ "id" => 25, "children" => [] }] }
-      ]
-
       item_removed = Space.new.remove_single_item_from_hierarchy!(23, hierarchy)
 
       # The document with id 23 is removed, children are moved level up
@@ -27,20 +65,10 @@ RSpec.describe Space, type: :model do
     end
 
     it "removes only one item" do
-      hierarchy = [
-        { "id" => 16, "children" => [] },
-        { "id" => 18, "children" => [] },
-        { "id" => 23, "children" => [{ "id" => 24, "children" => [
-          { "id" => 26, "children" => [] }
-        ] }] },
-        { "id" => 19, "children" => [{ "id" => 20, "children" => [] }] },
-        { "id" => 22, "children" => [{ "id" => 25, "children" => [] }] }
-      ]
-
-      item_removed = Space.new.remove_single_item_from_hierarchy!(16, hierarchy)
+      item_removed = Space.new.remove_single_item_from_hierarchy!(16, hierarchy_1)
 
       expect(item_removed).to be_truthy
-      expect(hierarchy).to eq([
+      expect(hierarchy_1).to eq([
         { "id" => 18, "children" => [] },
         { "id" => 23, "children" => [{ "id" => 24, "children" => [
           { "id" => 26, "children" => [] }
@@ -51,21 +79,11 @@ RSpec.describe Space, type: :model do
     end
 
     it "returns false it item was not found" do
-      hierarchy = [
-        { "id" => 16, "children" => [] },
-        { "id" => 18, "children" => [] },
-        { "id" => 23, "children" => [{ "id" => 24, "children" => [
-          { "id" => 26, "children" => [] }
-        ] }] },
-        { "id" => 19, "children" => [{ "id" => 20, "children" => [] }] },
-        { "id" => 22, "children" => [{ "id" => 25, "children" => [] }] }
-      ]
-
-      item_removed = Space.new.remove_single_item_from_hierarchy!(666, hierarchy)
+      item_removed = Space.new.remove_single_item_from_hierarchy!(666, hierarchy_2)
 
       # The document with id 23 is removed, children are moved level up
       expect(item_removed).to be_falsey
-      expect(hierarchy).to eq([
+      expect(hierarchy_2).to eq([
         { "id" => 16, "children" => [] },
         { "id" => 18, "children" => [] },
         { "id" => 23, "children" => [{ "id" => 24, "children" => [
@@ -79,24 +97,14 @@ RSpec.describe Space, type: :model do
 
   describe "#remove_item_with_children_from_hierarchy!" do
     it "removes only one item and returns it with children" do
-      hierarchy = [
-        { "id" => 16, "children" => [] },
-        { "id" => 18, "children" => [] },
-        { "id" => 23, "children" => [{ "id" => 24, "children" => [
-          { "id" => 26, "children" => [] }
-        ] }] },
-        { "id" => 19, "children" => [{ "id" => 20, "children" => [] }] },
-        { "id" => 22, "children" => [{ "id" => 25, "children" => [] }] }
-      ]
-
-      removed_item = Space.new.remove_item_with_children_from_hierarchy!(23, hierarchy)
+      removed_item = Space.new.remove_item_with_children_from_hierarchy!(23, hierarchy_3)
 
       # The document with id 23 is removed, children are moved level up
       expect(removed_item).to eq({ "id" => 23, "children" => [{ "id" => 24, "children" => [
         { "id" => 26, "children" => [] }
       ] }] })
 
-      expect(hierarchy).to eq([
+      expect(hierarchy_3).to eq([
         { "id" => 16, "children" => [] },
         { "id" => 18, "children" => [] },
         { "id" => 19, "children" => [{ "id" => 20, "children" => [] }] },
