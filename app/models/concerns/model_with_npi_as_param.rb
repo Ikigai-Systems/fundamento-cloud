a62170f9ea9cc9680642069b70e049619e91dfc4
Pawel Niewiadomski <11110000b@gmail.com>
2024-12-25 23:41:46 +0100

    You can now enable backwards compatibility to allow passing ids

--- app/models/concerns/model_with_npi_as_param.rb
+++ app/models/concerns/model_with_npi_as_param.rb
@@ -3,9 +3,21 @@ module ModelWithNpiAsParam
 
   include ModelWithNpi
 
+  included do
+    class_attribute :allow_fallback_to_id, default: false
+  end
+
   class_methods do
+    def set_allow_fallback_to_id(allowed)
+      self.allow_fallback_to_id = allowed
+    end
+
     def find_by_param!(param)
-      find_by_npi!(param)
+      if self.allow_fallback_to_id && !param.to_i.zero?
+        find_by_id!(param.to_i)
+      else
+        find_by_npi!(param)
+      end
     end
   end
 
