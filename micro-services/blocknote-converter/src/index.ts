17c7f74bd1190bceef987552b077d1a725166d6e
Pawel Niewiadomski <11110000b@gmail.com>
2025-01-03 16:19:17 +0100

    Rewrote index to use commander (supports two commands ATM)

--- micro-services/blocknote-converter/src/index.ts
+++ micro-services/blocknote-converter/src/index.ts
@@ -1,25 +1,93 @@
 import fs from "fs";
 import {convertToBlocks} from "./convertToBlocks";
+import path from "path";
+import {convertToYjs} from "./convertToYjs";
+import {Command} from "commander";
 
-async function readStdin() {
-  const chunks = [];
-  for await (const chunk of process.stdin) {
-    chunks.push(chunk);
-  }
-  return Buffer.concat(chunks).toString('utf8');
+const program = new Command();
+
+program
+  .name("blocknote-converter")
+  .description("CLI tool for converting YJS to blocknote and reverse");
+
+function createInputStream(input) {
+  const inputStream = input
+    ? fs.createReadStream(path.resolve(input))
+    : process.stdin;
+
+  inputStream.on('error', (err) => {
+    console.error(`Error reading input: ${err.message}`);
+    process.exit(1);
+  });
+
+  return inputStream;
 }
 
-const execute = async (process) => {
-  if (process.argv.length > 2) {
-    const params = process.argv.slice(2);
-    for (const param of params) {
-      const base64EncodedDiff = await fs.readFileSync(param, "utf-8");
-      console.log(JSON.stringify(convertToBlocks(base64EncodedDiff), null, 2));
-    }
-  } else {
-    const base64EncodedDiff = await readStdin();
-    console.log(JSON.stringify(convertToBlocks(base64EncodedDiff), null, 2));
-  }
+function createOutputStream(output) {
+  const outputStream = output
+    ? fs.createWriteStream(path.resolve(output))
+    : process.stdout;
+
+  outputStream.on('error', (err) => {
+    console.error(`Error writing output: ${err.message}`);
+    process.exit(1);
+  });
+  
+  return outputStream;
 }
 
-execute(process);
+program
+  .command("convert-yjs-to-blocks")
+  .description("Convert YJS to blocknote")
+  .option("-i, --input <file>", "Input file (default: stdin)")
+  .option("-o, --output <file>", "Output file (default: stdout)")
+  .action((options) => {
+    const inputStream = createInputStream(options.input);
+
+    const outputStream = createOutputStream(options.output);
+
+    const chunks = [];
+
+    inputStream.on('data', (chunk) => {
+      chunks.push(chunk);
+    });
+
+    inputStream.on('end', () => {
+      const convertedData = convertToBlocks(Buffer.concat(chunks));
+
+      outputStream.write(JSON.stringify(convertedData));
+
+      if (!options.output) {
+        outputStream.end();
+      }
+    });
+  });
+
+program
+  .command("convert-blocks-to-yjs")
+  .description("Convert Blocknote to YJS")
+  .option("-i, --input <file>", "Input file (default: stdin)")
+  .option("-o, --output <file>", "Output file (default: stdout)")
+  .action((options) => {
+    const inputStream = createInputStream(options.input);
+
+    const outputStream = createOutputStream(options.output);
+
+    const chunks = [];
+
+    inputStream.on('data', (chunk) => {
+      chunks.push(chunk);
+    });
+
+    inputStream.on('end', () => {
+      const convertedData = convertToYjs(JSON.parse(Buffer.concat(chunks).toString()));
+
+      outputStream.write(JSON.stringify(convertedData));
+
+      if (!options.output) {
+        outputStream.end();
+      }
+    });
+  });
+
+program.parse(process.argv);
