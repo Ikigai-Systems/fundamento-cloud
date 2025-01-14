6007e7dbb6cf1d211bb288f3c57924aa3659116f
Pawel Niewiadomski <11110000b@gmail.com>
2025-01-14 11:07:40 +0100

    Renamed the whole section

--- micro-services/formula-eval/test/tables.test.js
+++ micro-services/formula-eval/test/tables.test.js
@@ -2,7 +2,7 @@
 import { describe } from "mocha";
 import { testFormula } from "./formulaHelpers.js";
 
-describe('String related formulas', () => {
+describe('Table related formulas', () => {
   describe('CurrentRow', () => {
     testFormula(`Concatenate(CurrentRow("Key"), " ", CurrentRow("Name")))`, "JIRA Jira", {
       currentRow: {
