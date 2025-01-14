2727d262df104b9f544f3659608957900b2d4a58
Pawel Niewiadomski <11110000b@gmail.com>
2025-01-14 16:24:17 +0100

    s/API/Api/

--- app/services/formula_eval_gateway.rb
+++ app/services/formula_eval_gateway.rb
@@ -95,7 +95,7 @@ class FormulaEvalGateway
 
         table.add_row(nil, command["values"])
       when "DeleteRows"
-        table = API::V1::TablesController::find_relevant_table(command["tableNpi"], space.npi, organization_user)
+        table = Api::V1::TablesController::find_relevant_table(command["tableNpi"], space.npi, organization_user)
         command["tableNpi"] = table.npi # in case user provided table name, let's transform it to table id and provide it to frontend for caches invalidation
         # todo: validate the user is permitted to update this table
 
@@ -110,7 +110,7 @@ class FormulaEvalGateway
           row.destroy
         end
       when "AddOrUpdateRows"
-        table = API::V1::TablesController::find_relevant_table(command["tableNpi"], space.npi, organization_user)
+        table = Api::V1::TablesController::find_relevant_table(command["tableNpi"], space.npi, organization_user)
         command["tableNpi"] = table.npi # in case user provided table name, let's transform it to table id and provide it to frontend for caches invalidation
         # todo: validate the user is permitted to update this table
 
