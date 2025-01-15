6808c1b86b9102a5875d8567c90332bf17056dc6
Pawel Niewiadomski <11110000b@gmail.com>
2025-01-15 09:56:40 +0100

    Refactor Api::V1::TablesController to return JSON only

--- app/controllers/api/v1/tables_controller.rb
+++ app/controllers/api/v1/tables_controller.rb
@@ -10,11 +10,10 @@ class Api::V1::TablesController < Api::ApiController
       for_update: false
     )
 
-    respond_to do |format|
-      # ad json format: as an exception, frontend won't use camelCase -> snake_case deserialization of response payload from this endpoint
-      format.json { render json: { table: @table.attributes.except("space_id").merge({space_npi: @table.space.npi}), data: @table.data_to_json(evaluate_formulas: true, evaluate_as: current_organization_user) } }
-      format.all { head :unprocessable_content }
-    end
+    render json: {
+      table: @table.attributes.except("space_id").merge({space_npi: @table.space.npi}),
+      data: @table.data_to_json(evaluate_formulas: true, evaluate_as: current_organization_user)
+    }
   end
 
   def self.find_relevant_table(npi_or_name, space_npi, organization_user, for_update: true)
