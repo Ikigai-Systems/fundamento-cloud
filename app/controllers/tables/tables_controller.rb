94126621cf6877766c286c2d3992adb46a3b607c
Pawel Niewiadomski <11110000b@gmail.com>
2025-01-15 15:50:08 +0100

    Use the default policy_class as it's available now

--- app/controllers/tables/tables_controller.rb
+++ app/controllers/tables/tables_controller.rb
@@ -7,8 +7,7 @@ class Tables::TablesController < ApplicationController
   before_action :load_table, except: [:new, :create, :index]
 
   def index
-    @tables = policy_scope(current_organization.tables.lexicographically, policy_scope_class: DocumentPolicy::Scope)
-    @tables = @tables.where(space: @space)
+    @tables = policy_scope(current_organization.tables.lexicographically)
     @tables = @tables.where(space: @space) if @space.present?
     query = params[:query]
     @tables = @tables.where.like(name: "%#{query}%") if query.present?
