866fcb5e8eb4487de0f44c184153d93502a69a90
Piotr Stefaniak <stefan@ikigai.systems>
2025-01-22 15:47:18 +0100

    Make table of contents items clickable - they will scroll relevant block into viewport

--- app/javascript/components/TableOfContentsPanel.tsx
+++ app/javascript/components/TableOfContentsPanel.tsx
@@ -1,12 +1,22 @@
 import '@blocknote/mantine/style.css';
 import "./editor/editor-styles.css";
+import {useState} from "react";
+import useInterval from "../hooks/useInterval.ts";
 
 type TableOfContentsPanelProps = {
   content: unknown,
 }
 
 const TableOfContentsPanel = ({content}: TableOfContentsPanelProps) => {
-  const headerBlocks = content.filter(block => block.type === "heading" && block.content && block.content[0]?.text);
+  const [documentBlocks, setDocumentBlocks] = useState(window.blockNoteEditor?.document || content);
+
+  useInterval(() => {
+    if (window.blockNoteEditor) {
+      setDocumentBlocks(window.blockNoteEditor.document)
+    }
+  }, 1000);
+
+  const headerBlocks = documentBlocks.filter(block => block.type === "heading" && block.content && block.content[0]?.text);
 
   return (
     <div className="px-3">
@@ -14,7 +24,11 @@ const TableOfContentsPanel = ({content}: TableOfContentsPanelProps) => {
       {headerBlocks.length > 0 && (
         <ul>
           {headerBlocks.map(block => (
-            <li key={block.id} style={{marginLeft: `${(block.props.level - 1)}rem`}} className="p-1">
+            <li key={block.id} style={{marginLeft: `${(block.props.level - 1)}rem`}} className="p-1 cursor-pointer hover:underline"
+                onClick={() => {
+                  document.querySelector(`[data-id="${block.id}"]`)?.scrollIntoView({behavior: "smooth"});
+                }}
+            >
               {block.content[0].text}
             </li>
           ))}
