35df332bd2d84561b9517d3369963c1a1afbc75c
Piotr Stefaniak <stefan@ikigai.systems>
2025-01-30 12:48:21 +0100

    Make mentions tab to not blink when other user updates document

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,5 @@
 class RootController < ApplicationController
   def index
-    @mentions_count = MentionsExtractor::get_all_mentions(policy_scope(current_organization.documents), current_user).count
+    @mentions = MentionsExtractor::get_all_mentions(policy_scope(current_organization.documents), current_user)
   end
 end
