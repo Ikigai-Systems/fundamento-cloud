d1b103c3eba91fed5545f9cc49d485941f86dc0a
Piotr Stefaniak <stefan@ikigai.systems>
2025-02-11 16:48:38 +0100

    Add Sentry to formula-eval microservice

---
+++ micro-services/formula-eval/src/instrument.js
@@ -0,0 +1,19 @@
+import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+
+if (process.env.sentryDsn) {
+  Sentry.init({
+    dsn: process.env.sentryDsn,
+    integrations: [
+      nodeProfilingIntegration(),
+    ],
+
+    // Add Tracing by setting tracesSampleRate
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+
+    // Set sampling rate for profiling
+    // This is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
+}
--- micro-services/formula-eval/src/server.js
+++ micro-services/formula-eval/src/server.js
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/node";
 import Fastify from 'fastify';
 import FormBodyPlugin from '@fastify/formbody';
 import {fastifyRequestContext, requestContext} from '@fastify/request-context';
@@ -9,6 +10,8 @@ const fastify = Fastify({
 });
 fastify.register(FormBodyPlugin);
 fastify.register(fastifyRequestContext);
+
+Sentry.setupFastifyErrorHandler(fastify);
 
 fastify.addHook("onRequest", async (request, reply) => {
   const authorizationHeader = request.headers["authorization"];
@@ -26,21 +29,6 @@ fastify.post('/formulas/eval', async function handler (request, reply) {
 
   const {formula, additional_context, evaluation_context} = request.body;
   requestContext.set("evaluation_context", evaluation_context);
-
-  // // simulate formula evaluation via round-trip to fundamento formulas_controller.rb:
-  // const fundamentoFormulasEvalUrl = process.env.FUNDAMENTO_FORMULAS_EVAL_URL || "http://localhost:3000/formulas/eval"
-  // const response = await fetch(fundamentoFormulasEvalUrl, {
-  //   method: "POST",
-  //   headers: {
-  //     'Accept': 'application/json',
-  //     'Content-Type': 'application/x-www-form-urlencoded'
-  //   },
-  //   body: new URLSearchParams({
-  //     'formula': formula,
-  //     'additional_context': JSON.stringify(additional_context),
-  //   })
-  // });
-  // const responseJson = await response.json();
 
   try {
     const evaluatedFormula = evaluateFormula(formula, additional_context);
@@ -74,6 +62,6 @@ try {
 
   await fastify.listen({ host, port })
 } catch (err) {
-  fastify.log.error(err)
-  process.exit(1)
+  fastify.log.error(err);
+  process.exit(1);
 }
