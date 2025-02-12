b277b2b369f38f7c46f13a11f9fe9e111f9d9a19
Piotr Stefaniak <stefan@ikigai.systems>
2025-02-12 12:25:22 +0100

    Make ReactQuery not refetch upon switching back to active tab

--- app/javascript/contextes/ReactQueryClient.tsx
+++ app/javascript/contextes/ReactQueryClient.tsx
@@ -1,6 +1,12 @@
 import {QueryClient} from "@tanstack/react-query";
 
 // not a ReactContext per se, but behaving like one
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+  }
+});
 
 export default queryClient;
