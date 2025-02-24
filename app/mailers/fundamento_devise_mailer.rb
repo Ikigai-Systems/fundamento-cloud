b2adda80c7da4723009d9c37e0f141063cd8e08e
Piotr Stefaniak <stefan@ikigai.systems>
2025-02-24 13:28:48 +0100

    Invitation mails from, reply-to, subject headers will point at inviter.

---
+++ app/mailers/fundamento_devise_mailer.rb
@@ -0,0 +1,20 @@
+class FundamentoDeviseMailer < Devise::Mailer
+  protected
+
+  def subject_for(key)
+    return super  unless key == :invitation_instructions
+
+    "#{resource.invited_by.display_name} invited you to #{resource.organization.name}, an organization on Fundamento."
+  end
+
+  def headers_for(action, opts)
+    headers = super
+
+    return headers unless action == :invitation_instructions
+
+    headers[:reply_to] = "#{resource.invited_by.display_name} <#{resource.invited_by.email}>"
+    headers[:from] = "#{resource.invited_by.display_name} via Fundamento <no-reply@fundamento.it>"
+
+    headers
+  end
+end
