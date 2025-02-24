f93b5e221b147b4336c8c24393b6680559027172
Pawel Niewiadomski <11110000b@gmail.com>
2025-02-24 14:50:49 +0100

    Added mailer preview for invitation instructions, you can access it at http://localhost:3000/rails/mailers/devise_mailer/invitation_instructions in development mode [skip-tests]

---
+++ spec/mailers/previews/devise_mailer_preview.rb
@@ -0,0 +1,17 @@
+# test/mailers/previews/devise_mailer_preview.rb
+class DeviseMailerPreview < ActionMailer::Preview
+  def invitation_instructions
+    organization = Organization.new(name: "Test")
+
+    user = User.first
+
+    invited_user = InvitedUser.new(first_name: "Test", last_name: "User", email: "test@example.com")
+    invited_user.organization = organization
+    invited_user.invited_by = user
+    invited_user.invitation_sent_at = Time.now
+
+    token = "dummy_token"
+
+    Devise::Mailer.invitation_instructions(invited_user, token)
+  end
+end
