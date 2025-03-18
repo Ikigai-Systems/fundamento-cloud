48deacd4cfa6e84509112652158a41d151f580bc
Piotr Stefaniak <stefan@ikigai.systems>
2025-03-18 19:39:45 +0100

    Single loading state fake action in the command palette

--- app/javascript/stimulus/controllers/command_palette_controller.js
+++ app/javascript/stimulus/controllers/command_palette_controller.js
@@ -36,15 +36,17 @@ export default class CommandPaletteController extends Controller {
     }
 
     if (e.detail.search.length > 0) {
-      this.element.data = this.element.data.concat([{
-        id: "documentSearch_loading",
-        title: "Searching...",
-        section: "Documents",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="ninja-icon" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity="0.25"/><path fill="currentColor" d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"><animateTransform attributeName="transform" dur="0.75s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path></svg>`,
-        alwaysVisible: true,
-      }])
+      if (this.element.data.find(command => command.id === "documentSearch_loading") === undefined) {
+        this.element.data = this.element.data.concat([{
+          id: "documentSearch_loading",
+          title: "Searching...",
+          section: "Documents",
+          icon: `<svg xmlns="http://www.w3.org/2000/svg" class="ninja-icon" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity="0.25"/><path fill="currentColor" d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"><animateTransform attributeName="transform" dur="0.75s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path></svg>`,
+          alwaysVisible: true,
+        }]);
+      }
     } else {
-      this.element.data = this.element.data.filter(command => command.id !== 'documentSearch_loading');
+      this.element.data = this.element.data.filter(command => command.id !== "documentSearch_loading");
     }
 
     const documents = await DocumentsApi.index(); //todo: use e.detail.search as query parameter to narrow down documents in response
