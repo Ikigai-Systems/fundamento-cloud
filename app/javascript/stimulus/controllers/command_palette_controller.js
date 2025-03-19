1384b0ff372253c600b5eaa771e2cfd0663f4192
Piotr Stefaniak <stefan@ikigai.systems>
2025-03-19 12:27:33 +0100

    Slice too long document paths in command palette search results

--- app/javascript/stimulus/controllers/command_palette_controller.js
+++ app/javascript/stimulus/controllers/command_palette_controller.js
@@ -54,16 +54,20 @@ export default class CommandPaletteController extends Controller {
 
     this.element.data = this.element.data.filter(command => {
       return !command.id.startsWith("documentSearch#") && command.id !== "documentSearch_loading";
-    }).concat(results.map(({document, space}) => ({
-      id: `documentSearch#${document.npi}`,
-      value: document.title,
-      title: `${space.name} ⎯ ${document.parentPath}${document.title}`,
-      section: "Documents",
-      icon: '<svg xmlns="http://www.w3.org/2000/svg" class="ninja-icon" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9"/></svg>',
-      handler: () => {
-        Turbo.visit(DocumentsApi.show.path({npi: document?.npi}));
-      }
-    })));
+    }).concat(results.map(({document, space}) => {
+      const documentTitleWithPath = document.parentPath + document.title;
+      const displayTitle = `${space.name} ⎯ ${documentTitleWithPath.length > 60 ? "..." : ""}${(documentTitleWithPath).slice(-60)}`;
+      return {
+        id: `documentSearch#${document.npi}`,
+        value: document.title,
+        title: displayTitle,
+        section: "Documents",
+        icon: '<svg xmlns="http://www.w3.org/2000/svg" class="ninja-icon" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9"/></svg>',
+        handler: () => {
+          Turbo.visit(DocumentsApi.show.path({npi: document?.npi}));
+        }
+      };
+    }));
     this._cachedResults = true;
   }
 
