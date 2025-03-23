b0b6add4fda2ae85407bde34e95e926c81b68677
Pawel Niewiadomski <11110000b@gmail.com>
2025-03-23 16:36:55 +0100

    Don't initialize recaptcha if there are no secrets set

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,6 @@
-Recaptcha.configure do |config|
-  config.site_key  = Rails.application.credentials.recaptcha.site_key!
-  config.secret_key = Rails.application.credentials.recaptcha.secret_key!
+if Rails.application.credentials.recaptcha
+  Recaptcha.configure do |config|
+    config.site_key  = Rails.application.credentials.recaptcha.site_key!
+    config.secret_key = Rails.application.credentials.recaptcha.secret_key!
+  end
 end
