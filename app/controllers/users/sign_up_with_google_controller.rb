6aa458730275cea5002adfd455e3c4aa9a8109be
Piotr Stefaniak <stefan@ikigai.systems>
2025-04-26 11:54:20 +0200

    Remove unneccessary logging of request headers

--- app/controllers/users/sign_up_with_google_controller.rb
+++ app/controllers/users/sign_up_with_google_controller.rb
@@ -8,10 +8,7 @@ class Users::SignUpWithGoogleController < ApplicationController
   def sign_up_with_google
     Sentry.capture_message("Attempt to sign up with Google", level: :info, extra: {
       ip_address: request.remote_ip
-    }.merge(
-      self.request.env.select { |k, v| k =~ /^HTTP_/ }
-    ))
-
+    })
     flash[:warning] = "Sign up with Google is under construction, please use email registration in the meanwhile."
     redirect_to new_user_registration_path
   end
