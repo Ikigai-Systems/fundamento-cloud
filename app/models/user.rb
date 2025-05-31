3abcaf5ea78a076744901f6a6d14075562a8e69e
Piotr Stefaniak <stefan@ikigai.systems>
2025-05-31 10:47:45 +0200

    Poprawiam po Claude gdzie ma być sekcja `private` w modelu user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,22 +19,6 @@ class User < ApplicationRecord
 
   validate :password_complexity, if: :password_required?
 
-  private
-
-  def password_complexity
-    return if password.blank?
-
-    errors.add(:password, 'must be at least 8 characters long') if password.length < 8
-    errors.add(:password, 'must contain at least one uppercase letter') unless password.match?(/[A-Z]/)
-    errors.add(:password, 'must contain at least one lowercase letter') unless password.match?(/[a-z]/)
-    errors.add(:password, 'must contain at least one number') unless password.match?(/\d/)
-    errors.add(:password, 'must contain at least one special character') unless password.match?(/[!@#$%^&*()_+{}\[\]:;<>,.?~\\|-]/)
-  end
-
-  def password_required?
-    !persisted? || !password.nil? || !password_confirmation.nil?
-  end
-
   def initials
     first_name.first(1) + last_name.first(1)
   end
@@ -64,4 +48,21 @@ class User < ApplicationRecord
   def send_reset_password_instructions
     super if invitation_token.nil?
   end
+
+  private
+
+  def password_complexity
+    return if password.blank?
+
+    errors.add(:password, 'must be at least 8 characters long') if password.length < 8
+    errors.add(:password, 'must contain at least one uppercase letter') unless password.match?(/[A-Z]/)
+    errors.add(:password, 'must contain at least one lowercase letter') unless password.match?(/[a-z]/)
+    errors.add(:password, 'must contain at least one number') unless password.match?(/\d/)
+    errors.add(:password, 'must contain at least one special character') unless password.match?(/[!@#$%^&*()_+{}\[\]:;<>,.?~\\|-]/)
+  end
+
+  def password_required?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
+
 end
