7964c8445eee612e19f5e8c80bfe711af22e79d4
Pawel Niewiadomski <11110000b@gmail.com>
2025-06-13 11:20:02 +0200

    Refactor organization_user_avatar_component.rb so it accepts user/organization as well

--- app/components/organization_user_avatar_component.rb
+++ app/components/organization_user_avatar_component.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 class OrganizationUserAvatarComponent < ViewComponent::Base
-  def initialize(organization_user:)
-    @organization_user = organization_user
+  def initialize(organization_user: nil, user: nil, organization: nil)
+    if organization_user.present?
+      @user = organization_user.user
+      @organization = organization_user.organization
+    else
+      @user = user
+      @organization = organization
+    end
   end
 
   def online?
-    @organization_user.online?
+    @organization.presence && @user.online?(@organization)
   rescue
     false
   end
