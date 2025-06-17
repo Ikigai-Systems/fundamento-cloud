8f0f6baff1b1c4b0e8db4210b93924571e2a8ec2
Pawel Niewiadomski <11110000b@gmail.com>
2025-06-17 12:27:43 +0200

    It's possible to specify avatar's size

--- app/components/user_avatar.rb
+++ app/components/user_avatar.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class UserAvatar < ViewComponent::Base
-  def initialize(organization_user: nil, user: nil, organization: nil)
+  def initialize(organization_user: nil, user: nil, organization: nil, size: "md")
+    @size = size.to_sym
+
     if organization_user.present?
       @user = organization_user.user
       @organization = organization_user.organization
@@ -15,5 +17,21 @@ class UserAvatar < ViewComponent::Base
     @organization.presence && @user.online?(@organization)
   rescue
     false
+  end
+
+  def size_to_dimensions
+    mapping = {
+      xs: 16,
+      sm: 24,
+      md: 32,
+      lg: 64,
+      xl: 128
+    }
+
+    mapping[@size] || mapping[:xs]
+  end
+
+  def size_to_class
+    "size-#{size_to_dimensions}"
   end
 end
