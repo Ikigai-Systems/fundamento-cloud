ae8e1241f08672c7c12d8fc2228a8d927bd13bec
Pawel Niewiadomski <11110000b@gmail.com>
2025-09-04 09:20:36 +0200

    Fix missing OpenStruct after fast-mcp was added

--- config/initializers/js_from_routes.rb
+++ config/initializers/js_from_routes.rb
@@ -1,6 +1,8 @@
+require "ostruct"
+
 if Rails.env.development?
   # Example: Generate TypeScript files.
   JsFromRoutes.config do |config|
-    config.client_library = '@js-from-routes/axios'
+    config.client_library = "@js-from-routes/axios"
   end
 end
