d50fce2d91ba24af8514d78924aa9ddc20ff9828
Pawel Niewiadomski <11110000b@gmail.com>
2025-09-30 17:20:42 +0200

    Fixed multiselect to correctly merge query params if they are included in searchUrl

--- app/javascript/stimulus/index.js
+++ app/javascript/stimulus/index.js
@@ -28,7 +28,25 @@ class FixedPopover extends Popover {
 application.register('popover', FixedPopover);
 
 import { Multiselect } from '@wizardhealth/stimulus-multiselect'
-application.register('multiselect', Multiselect);
+
+class FixedMultiselect extends Multiselect {
+  async searchRemote() {
+    if (this.searchTarget.value === "") return
+
+    const url = new URL(this.searchUrlValue, window.location.origin)
+    url.searchParams.set("q", this.searchTarget.value)
+    url.searchParams.set("preselects", this.selectedValue.map(x => x.value).join(","))
+
+    const response = await fetch(url.toString())
+
+    const searchedItems = await response.json()
+
+    this.itemsValue = searchedItems
+    this.dropdownTarget.classList.add("multiselect__dropdown--open")
+  }
+}
+
+application.register('multiselect', FixedMultiselect);
 
 import ReactLoader from "./react_loader_controller"
 application.register("react-loader", ReactLoader)
