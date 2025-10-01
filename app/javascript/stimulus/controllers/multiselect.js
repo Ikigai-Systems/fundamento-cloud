5c1d915bc9858f30957ac3a04773ab8ae81620f9
Pawel Niewiadomski <11110000b@gmail.com>
2025-10-01 12:13:00 +0200

    Updated multiselect to add X-CSRF-Token

--- app/javascript/stimulus/controllers/multiselect.js
+++ app/javascript/stimulus/controllers/multiselect.js
@@ -14,4 +14,26 @@ export default class FixedMultiselect extends Multiselect {
     this.itemsValue = searchedItems
     this.dropdownTarget.classList.add("multiselect__dropdown--open")
   }
+
+  async addable(e) {
+    e.preventDefault()
+    const query = this.searchTarget.value
+
+    if (query === "" || this.itemsValue.some(item => item.text === query)) return
+
+    const response = await fetch(this.addableUrlValue, {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": document.querySelector("[name='csrf-token']").content,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ addable: query })
+    })
+
+    if (response.ok) {
+      const addedItem = await response.json()
+
+      this.addAddableItem(addedItem)
+    }
+  }
 }
