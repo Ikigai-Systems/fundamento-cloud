82dc6c54ba72e0543f7c6069af23ee69249fa493
Pawel Niewiadomski <11110000b@gmail.com>
2025-10-06 17:05:20 +0200

    Added TagsControllers

---
+++ app/controllers/objects/tags_controller.rb
@@ -0,0 +1,31 @@
+class Objects::TagsController < ApplicationController
+  include EnsureOrganization
+
+  include LoadDocument.from_param(:document_npi)
+  include LoadTable.from_param(:table_npi)
+
+  after_action :verify_authorized
+
+  before_action :load_document, if: -> { params[:document_npi].present? }
+  before_action :load_table, if: -> { params[:table_npi].present? }
+  before_action -> { @object = @document || @table }
+
+  def create
+    authorize @object, :update?
+
+    tags_service = TagsService.new(object: @object, organization: current_organization)
+
+    tags = tags_service.update_tags(params[:tags]).map do
+      {
+        :text => _1.hashtag,
+        :value => _1.hashtag
+      }
+    end
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace([@object, :tags], SidebarTags.new(object: @object))
+      end
+    end
+  end
+end
