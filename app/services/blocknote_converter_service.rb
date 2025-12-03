41cc57f92b97aa25fe2084b6899c26669cddd21e
Pawel Niewiadomski <11110000b@gmail.com>
2025-12-03 15:21:45 +0100

    Build creates blocknoteConverter.cjs now

--- app/services/blocknote_converter_service.rb
+++ app/services/blocknote_converter_service.rb
@@ -2,7 +2,7 @@ module BlocknoteConverterService
   def self.to_blocks(binary_sync)
     # Call the Node.js script and pass the JSON data as an argument
     stdout, stderr, status = Open3.capture3(
-      'node ./micro-services/blocknote-converter/build/blocknoteConverter.js convert-yjs-to-blocks',
+      'node ./micro-services/blocknote-converter/build/blocknoteConverter.cjs convert-yjs-to-blocks',
       binmode: true,
       stdin_data: binary_sync
     )
