92e6dd54b50159edd55552d1119ea1631a13f116
Pawel Niewiadomski <11110000b@gmail.com>
2025-12-24 14:41:26 +0100

    Tweaking minio-init.sh

--- dockerfiles/minio-init.sh
+++ dockerfiles/minio-init.sh
@@ -2,10 +2,13 @@
 
 set -xe
 
+# Set up MinIO alias first
+mc alias set minio http://${MINIO_HOST}:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+
+# Check if bucket already exists
 if mc ls minio/fundamento-development > /dev/null 2>&1; then
   echo "Bucket already exists, skipping initialization."
 else
-  mc alias set minio http://${MINIO_HOST}:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
   mc mb minio/fundamento-development
   mc admin user add minio ${MINIO_ACCESS_KEY:-fundamento} ${MINIO_SECRET_KEY:-IPaWkaUi9Ko1NA}
   mc admin policy attach minio readwrite --user ${MINIO_ACCESS_KEY:-fundamento}
