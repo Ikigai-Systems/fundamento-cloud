bd9c944541c0206097dcf71e1aa5deed90912d0e
Pawel Niewiadomski <11110000b@gmail.com>
2025-12-25 11:31:17 +0100

    Get host from Rails configuration

--- spec/e2e/cypress/app_commands/user_confirmation_url.rb
+++ spec/e2e/cypress/app_commands/user_confirmation_url.rb
@@ -7,7 +7,7 @@ if user && user.confirmation_token.present?
   # This uses the user_confirmation_url helper provided by Devise
   Rails.application.routes.url_helpers.user_confirmation_url(
     confirmation_token: user.confirmation_token,
-    host: ENV.fetch("CYPRESS_BASE_URL", "localhost:4000")
+    host: Rails.application.config.action_mailer.default_url_options.fetch(:host, "localhost:4000")
   )
 else
   raise "User not found or confirmation token not present for email: #{email}"
