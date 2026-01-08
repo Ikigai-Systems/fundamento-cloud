2aa8c5ef6894ef0397e0604da705e875352d1dce
Pawel Niewiadomski <11110000b@gmail.com>
2026-01-08 16:47:34 +0100

    ConfirmationsController calls super in after_confirmation_path_for (seems to be a better choice)

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -9,6 +9,6 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
 
   def after_confirmation_path_for(resource_name, resource)
     sign_in(resource)
-    root_path
+    super
   end
 end
