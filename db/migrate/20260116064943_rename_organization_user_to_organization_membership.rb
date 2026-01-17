5b8d556abc24a042541a3b22f2461ddffc326bf6
Pawel Niewiadomski <11110000b@gmail.com>
2026-01-17 10:03:02 +0100

    Fix migration

--- db/migrate/20260116064943_rename_organization_user_to_organization_membership.rb
+++ db/migrate/20260116064943_rename_organization_user_to_organization_membership.rb
@@ -1,18 +1,18 @@
 class RenameOrganizationUserToOrganizationMembership < ActiveRecord::Migration[8.1]
   def up
     # Rename main table
-    rename_table :organization_memberships, :organization_memberships
+    rename_table :organization_users, :organization_memberships
 
     # Rename related table
-    rename_table :organization_membership_properties, :organization_membership_properties
+    rename_table :organization_users_properties, :organization_membership_properties
 
     # Rename foreign key columns in dependent tables
-    rename_column :api_tokens, :organization_membership_id, :organization_membership_id
-    rename_column :favorites, :organization_membership_id, :organization_membership_id
-    rename_column :object_reactions, :organization_membership_id, :organization_membership_id
-    rename_column :object_comments, :organization_membership_id, :organization_membership_id
-    rename_column :document_imports, :organization_membership_id, :organization_membership_id
-    rename_column :organization_membership_properties, :organization_membership_id, :organization_membership_id
+    rename_column :api_tokens, :organization_users_id, :organization_membership_id
+    rename_column :favorites, :organization_users_id, :organization_membership_id
+    rename_column :object_reactions, :organization_users_id, :organization_membership_id
+    rename_column :object_comments, :organization_users_id, :organization_membership_id
+    rename_column :document_imports, :organization_users_id, :organization_membership_id
+    rename_column :organization_membership_properties, :organization_users_id, :organization_membership_id
 
     # Update polymorphic type strings in space_memberships
     execute <<-SQL
@@ -45,17 +45,17 @@ class RenameOrganizationUserToOrganizationMembership < ActiveRecord::Migration[8
     SQL
 
     # Rollback foreign key column renames
-    rename_column :organization_membership_properties, :organization_membership_id, :organization_membership_id
-    rename_column :document_imports, :organization_membership_id, :organization_membership_id
-    rename_column :object_comments, :organization_membership_id, :organization_membership_id
-    rename_column :object_reactions, :organization_membership_id, :organization_membership_id
-    rename_column :favorites, :organization_membership_id, :organization_membership_id
-    rename_column :api_tokens, :organization_membership_id, :organization_membership_id
+    rename_column :organization_membership_properties, :organization_membership_id, :organization_users_id
+    rename_column :document_imports, :organization_membership_id, :organization_users_id
+    rename_column :object_comments, :organization_membership_id, :organization_users_id
+    rename_column :object_reactions, :organization_membership_id, :organization_users_id
+    rename_column :favorites, :organization_membership_id, :organization_users_id
+    rename_column :api_tokens, :organization_membership_id, :organization_users_id
 
     # Rollback related table rename
-    rename_table :organization_membership_properties, :organization_membership_properties
+    rename_table :organization_membership_properties, :organization_users_properties
 
     # Rollback main table rename
-    rename_table :organization_memberships, :organization_memberships
+    rename_table :organization_memberships, :organization_users
   end
 end
