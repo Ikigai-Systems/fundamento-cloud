db76643225b3b7351d5d8239be748111c63cf314
Pawel Niewiadomski <11110000b@gmail.com>
2026-01-18 16:33:49 +0100

    Making sure table of contents in the sidebar is updated when editor content is changing

--- app/components/sidebar_tab_icon.rb
+++ app/components/sidebar_tab_icon.rb
@@ -1,17 +1,18 @@
 class SidebarTabIcon < ViewComponent::Base
   erb_template <<-ERB
-    <div data-controller="popover" class="relative" data-action="mouseenter->popover#show mouseleave->popover#hide" >
+    <div data-controller="popover" class="relative" data-action="mouseenter->popover#show mouseleave->popover#hide" aria-label="<%= @label %>">
       <%= content %>
 
       <template data-popover-target="content">
         <div class="popover-tooltip-card m-1" data-popover-target="card">
-          <p><%= @tooltip %></p>
+          <p><%= @tooltip || @label %></p>
         </div>
       </template>
     </div>
   ERB
 
-  def initialize(tooltip:)
+  def initialize(label:, tooltip: nil)
     @tooltip = tooltip
+    @label = label
   end
 end
--- spec/e2e/cypress/e2e/documents/document-editor.cy.js
+++ spec/e2e/cypress/e2e/documents/document-editor.cy.js
@@ -1,5 +1,7 @@
 import {isOrganizationCookie} from "../../support/organization-cookies.js";
 
+const editPageUrl = /\/d\/[_\-a-zA-Z0-9]+\/edit$/;
+
 describe("Document Editor", function () {
   beforeEach(() => {
     cy.app("clean");
@@ -30,7 +32,7 @@ describe("Document Editor", function () {
     cy.get('[aria-label="Create new document"]').click();
 
     // Wait for the document to be created and redirected to edit page
-    cy.url().should("match", /\/d\/[a-zA-Z0-9]+\/edit$/);
+    cy.url().should("match", editPageUrl);
 
     // Verify the editor is loaded
     cy.get("[data-document-editor]").should("exist");
@@ -59,4 +61,97 @@ describe("Document Editor", function () {
     cy.get("[data-document-editor] .bn-block-outer").should("have.length.at.least", 2);
   });
 
+  it("displays headers in the sidebar hierarchy tab and updates when document changes", function () {
+    // Create a new document with headers
+    cy.visit("/s/is_default");
+    cy.get('[aria-label="Create new document"]').click();
+
+    // Wait for redirect to edit page
+    cy.url().should("match", editPageUrl);
+
+    // Wait for editor to load
+    cy.get("[data-document-editor]").should("exist");
+
+    // Type the heading text
+    cy.get("[data-document-editor] [role=\"textbox\"]").click();
+
+    cy.focused().type("# First Heading{enter}");
+    cy.focused().type("## Second Heading{enter}");
+    cy.focused().type("Some content under the second heading.");
+
+    // Verify headings are visible in the editor
+    cy.contains("First Heading").should("be.visible");
+    cy.contains("Second Heading").should("be.visible");
+
+    cy.get("#content-sidebar").within(() => {
+      cy.get('[aria-label="Structure"]').click();
+    });
+
+    // Verify headers appear in the hierarchy
+    cy.get('#table_of_contents_sidebar_tab').within(() => {
+      cy.contains("First Heading").should("be.visible");
+      cy.contains("Second Heading").should("be.visible");
+    });
+
+    // Update a heading
+    cy.contains("First Heading").click();
+    cy.focused().type("{selectall}Updated First Heading");
+
+    // Verify the hierarchy updates
+    cy.get('#table_of_contents_sidebar_tab').within(() => {
+      cy.contains("Updated First Heading").should("be.visible");
+      cy.contains("Second Heading").should("not.exist");
+    });
+  });
+
+  it("saves multiple versions correctly and displays them in version history", function () {
+    // Get existing document ID
+    cy.appEval("Document.first.id").then((documentId) => {
+      cy.visit(`/d/${documentId}`);
+
+      // Wait for editor to load
+      cy.get("[data-document-editor]").should("exist");
+
+      // Get initial version count
+      cy.appEval(`Document.find('${documentId}').versions.count`).then((initialVersionCount) => {
+        // Make first edit
+        cy.get("[data-document-editor] [role=\"textbox\"]").first().type("First version content.{enter}");
+
+        // Save the document by clicking the update button
+        cy.get('[aria-label="Save document"]').click();
+
+        // Wait for save confirmation
+        cy.contains("Document has been updated").should("be.visible");
+
+        // Verify version was created
+        cy.appEval(`Document.find('${documentId}').versions.count`).then((newCount) => {
+          expect(newCount).to.equal(initialVersionCount + 1);
+        });
+
+        // Make second edit
+        cy.get("[data-document-editor] [role=\"textbox\"]").last().type("Second version content.{enter}");
+
+        // Save again
+        cy.get('[aria-label="Save document"]').click();
+        cy.contains("Document has been updated").should("be.visible");
+
+        // Verify another version was created
+        cy.appEval(`Document.find('${documentId}').versions.count`).then((finalCount) => {
+          expect(finalCount).to.equal(initialVersionCount + 2);
+        });
+
+        // Make third edit
+        cy.get("[data-document-editor] [role=\"textbox\"]").last().type("Third version content.");
+
+        // Save again
+        cy.get('[aria-label="Save document"]').click();
+        cy.contains("Document has been updated").should("be.visible");
+
+        // Verify third version was created
+        cy.appEval(`Document.find('${documentId}').versions.count`).then((thirdCount) => {
+          expect(thirdCount).to.equal(initialVersionCount + 3);
+        });
+      });
+    });
+  });
 });
