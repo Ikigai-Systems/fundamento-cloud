acc156e442dc6a9138e308b2a189334164f0b7ea
Pawel Niewiadomski <11110000b@gmail.com>
2026-01-23 11:27:36 +0100

    Fixed sidebar_connections_tab.rb referencing object_npi

--- app/components/sidebar_connections_tab.rb
+++ app/components/sidebar_connections_tab.rb
@@ -15,10 +15,10 @@ class SidebarConnectionsTab < ViewComponent::Base
     @references = ReferencesExtractor::all_references(Pundit.policy_scope(@pundit_user, @pundit_user.current_organization.documents))
 
     # Because some objects might be referenced by ID/NPI we need to make sure we show them once
-    @references.uniq! { |reference| [reference.referenced_by, reference.object_type, reference.object_npi] }
+    @references.uniq! { |reference| [reference.referenced_by, reference.object_type, reference.object_id] }
 
     @incoming = @references.select do |reference|
-      reference.object_type == @object.class.to_s && reference.object_npi == @object.npi
+      reference.object_type == @object.class.to_s && reference.object_id == @object.id
     end.map { |reference| with_link_details(reference) }
 
     @outgoing = @references.select do |reference|
@@ -32,11 +32,11 @@ class SidebarConnectionsTab < ViewComponent::Base
     reference.tap do |reference|
       case reference.object_type
       when "Table"
-        reference.object_title = @pundit_user.current_organization.tables.select(:name).find_by_param!(reference.object_npi).name
-        reference.object_path = table_path(reference.object_npi)
+        reference.object_title = @pundit_user.current_organization.tables.select(:name).find_by_param!(reference.object_id).name
+        reference.object_path = table_path(reference.object_id)
       when "Document"
-        reference.object_title = @pundit_user.current_organization.documents.select(:title).find_by_param!(reference.object_npi).title
-        reference.object_path = document_path(reference.object_npi)
+        reference.object_title = @pundit_user.current_organization.documents.select(:title).find_by_param!(reference.object_id).title
+        reference.object_path = document_path(reference.object_id)
       else
         raise ArgumentError.new("Unrecognized object type: #{reference.object_type}")
       end
